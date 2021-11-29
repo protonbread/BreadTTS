1fd68fd08a133b84bcb680d6290aba9aa121cc81
protonbread <b3nc@protonmail.com>
2021-11-29 18:01:38 -0500

    V.1

--- main.py
+++ main.py
@@ -7,18 +7,13 @@ import itertools
 os.system('cls' if os.name == 'nt' else 'clear')
 
 resolution = '1080x1920'
-batch = 10
-commentbatch = 2
+batch = 30
+comment_batch = 2
 
-postdict = {}
+post_dict = {}
 titles = []
-
-disallowedids = []
-dirtypostids = []
-filteredpostids = []
-
-urls = []
-filteredurls = []
+post_ids = []
+post_urls = []
 
 s = HTMLSession()
 
@@ -30,22 +25,18 @@ github.com/protonbread
 ═══════════════════════
 [Resolution: {resolution}]
 [Post Count: {batch}]
-[Comment Count: {commentbatch}]
+[Comment Count: {comment_batch}]
 ═══════════════════════
 """)
-print('[%]Scraping Reddit...')
+
 url = 'https://gateway.reddit.com/desktopapi/v1/subreddits/AskReddit?rtj=only&redditWebClient=web2x&app=web2x-client-production&allow_over18=&include=prefsSubreddit&dist=8&forceGeopopular=false&layout=card&sort=hot'
 r = s.get(url)
-postjson = r.json()
-
-for i in itertools.islice(postjson['posts'], batch):
-    post = postjson['posts'][i]['title']
-    url = postjson['posts'][i]['permalink']
-    postid = postjson['posts'][i]['id']
-
-    dirtypostids.append(postid)
-    titles.append(post)
-    urls.append(url)
+post_json = r.json()
+print('[%]Scraping Reddit...')
+for i in itertools.islice(post_json['posts'], batch):
+    titles.append(post_json['posts'][i]['title'])
+    post_urls.append(post_json['posts'][i]['permalink'])
+    post_ids.append(post_json['posts'][i]['id'])
 
 donotreadlist = open('./donotread.txt', 'a+')
 alreadyread = open('./alreadyread.txt', 'a+')
@@ -54,30 +45,27 @@ donotread = [item for item, count in collections.Counter(titles).items() if coun
 titles = [elem for elem in titles if elem not in donotread]
 
 #Ad url's vary so we can't check for duplicates
-for i in urls:
+
+for i in post_urls[:]:
     if 'https://www.reddit.com/user/' in i:
-        badid = i.split('/')
-        disallowedids.append(f't3_{badid[6]}')
-    else:
-        filteredurls.append(i)
+        post_urls.remove(i)
+        badid = i.split('/')[6]
+        try:
+            post_ids.remove(f't3_{badid}')
+        except:
+            pass
 
-for i in dirtypostids:
+for i in post_ids[:]:
     if '=' in i:
-        pass
-    else:
-        filteredpostids.append(i)
+        post_ids.remove(i)
 
-for i in filteredpostids:
-    if i in disallowedids:
-        filteredpostids.remove(i)
+lastid = post_ids[len(post_ids)-1]
 
-lastid = filteredpostids[len(filteredpostids)-1]
+for (posturl, posttitle) in zip(post_urls, titles):
+    post_dict.update({posturl: posttitle})
 
-for (postid, posttitle) in zip(filteredpostids, titles):
-    postdict.update({postid: posttitle})
-
-for i in filteredurls:
-    pass
+#for i in filteredurls:
+#    pass
 
 
 print(f"[!]Got {len(titles)} Posts And ")
